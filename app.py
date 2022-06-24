70b028190c59f8887866aa8b3fc857dcf4f3ded7
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2022-06-24 20:21:13 +0530

    testing update method

--- app.py
+++ app.py
@@ -32,6 +32,33 @@ def get_all_users():
     return jsonify(users)
 
 
+# user update ---------------------------------------
+@app.route('/user_update', methods=["PATCH"])
+def user_update():
+    user_updates = json.loads(request.data)
+    # user_up_id = user_updates['_id']
+    name = user_updates['name']
+    email = user_updates['email']
+    district = user_updates['district']
+
+    mongo_db.USER.update_one({"email": email}, {"$set": {
+        "name": name,
+        "district": district,
+    }})
+
+    return "update user"
+
+
+# user delete ---------------------------------------
+@app.route('/user_delete', methods=["DELETE"])
+def user_delete():
+    user_deletes = json.loads(request.data)
+    user_del_id = user_deletes['_id']
+    mongo_db.USER.delete_one({"_id": user_del_id})
+    return "User Delete"
+
+
+
 
 
 if __name__ == '__main__':
