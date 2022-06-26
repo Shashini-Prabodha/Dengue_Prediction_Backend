89b14a8aebc1e6d32757eaec7a5fdc9cda4d5bc9
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2022-06-26 13:18:19 +0530

    signup and login  issue fixed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, json, jsonify, request, session
 
-
 from config import mongo_db
 
 app = Flask(__name__)
@@ -17,35 +16,34 @@ def root():
 def sign_up():
     user_sign_up_data = json.loads(request.data)
     print("LOG ==> ", user_sign_up_data)
-    # id = mongo_db.USER.count_documents({}) + 1
-    #
-    # record = {"_id": id,
-    #           "email": user_sign_up_data['email'],
-    #           "name": user_sign_up_data['name'],
-    #           "password": user_sign_up_data["password"],
-    #           "district": user_sign_up_data['district'],
-    #           }
-    # mongo_db.USER.insert_one(record)
+    id = mongo_db.USER.count_documents({}) + 1
+
+    record = {"_id": id,
+              "email": user_sign_up_data['email'],
+              "name": user_sign_up_data['name'],
+              "password": user_sign_up_data["password"],
+              "district": user_sign_up_data['district'],
+              }
+    mongo_db.USER.insert_one(record)
 
     mongo_db.USER.insert_one(user_sign_up_data)
     return "User Sign Up"
 
+
 @app.route('/login_user', methods=["GET"])
 def search_user_login():
-
     req_user_email = request.args["email"]
     # print(req_user_id)
     # search_id = req_user_email['email']
     print(req_user_email)
     search_user_details = mongo_db.USER.find_one({"email": req_user_email})
     print(type(search_user_details))
+    return jsonify(search_user_details)
+    # return search_user_details
 
-    # return jsonify(search_user_details)
-    return search_user_details
 
 @app.route('/search_user', methods=["GET"])
 def search_user():
-
     req_user_id = json.loads(request.data)
     print(req_user_id)
     search_id = req_user_id['email']
@@ -56,10 +54,10 @@ def search_user():
     # return jsonify(search_user_details)
     return search_user_details
 
+
 # get all user  ------------------------------------
 @app.route('/all_users', methods=["GET"])
 def get_all_users():
-
     get_db_users = mongo_db.USER.find()
     users = []
     for data in get_db_users:
@@ -72,12 +70,11 @@ def get_all_users():
 # user update ---------------------------------------
 @app.route('/update_user', methods=["PATCH"])
 def update_user():
-
     user_updates = json.loads(request.data)
     name = user_updates['name']
     email = user_updates['email']
     district = user_updates['district']
-    print('email ',email)
+    print('email ', email)
     mongo_db.USER.update_one({"email": email}, {"$set": {
         "name": name,
         "district": district
@@ -93,9 +90,6 @@ def delete_user():
     user_del_id = user_deletes['email']
     mongo_db.USER.delete_one({"email": user_del_id})
     return "User Delete"
-
-
-
 
 
 if __name__ == '__main__':
